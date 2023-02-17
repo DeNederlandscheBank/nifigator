1b0c064169130e9819608f88146c83fea31f1aff
Willem Jan Willemse <w.j.willemse@dnb.nl>
2023-02-17 12:43:25 +0100

    fix for version

--- src/nifigator/__init__.py
+++ src/nifigator/__init__.py
@@ -13,7 +13,7 @@ try:
     dist_name = __name__
     __version__ = version(dist_name)
 except PackageNotFoundError:  # pragma: no cover
-    __version__ = "0.1.13"
+    __version__ = "0.1.16"
 finally:
     del version, PackageNotFoundError
 
